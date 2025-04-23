b48c948924c322c57e3f864692c9a7e1460a7140
Abhishek Bhatia <abhatia2107@gmail.com>
2025-04-23 21:09:40 +0530

    Update About and Hero sections, added links to projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Navbar from "./components/navbar";
 
 const App = () => {
   return (
